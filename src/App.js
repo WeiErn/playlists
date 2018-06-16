2423e1c9cf64010c32e26f676146ee14f7e0e5a3
Wei Ern <ng.wei.ern@gmail.com>
2018-06-16 22:25:27 +0800

    replaced fake server data with actual Spotify data from the signed in user

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import queryString from 'query-string';
 
 let defaultTextColor = '#fff';
 let defaultStyle = {
@@ -90,7 +91,7 @@ class Playlist extends Component {
         let playlist = this.props.playlist;
         return(
             <div style={{...defaultStyle, display: 'inline-block', width: "25%"}}>
-                <img />
+                <img src={playlist.imageUrl} style={{width: '160px'}}/>
                 <h3>{playlist.name}</h3>
                 <ul>
                     {playlist.songs.map(song =>
@@ -112,37 +113,63 @@ class App extends Component {
     }
 
     componentDidMount() {
-        setTimeout(() => {
-            this.setState({serverData: fakeServerData});
-        }, 1000);
+        let parsed = queryString.parse(window.location.search);
+        let accessToken = parsed.access_token;
+
+        fetch('https://api.spotify.com/v1/me', {
+            headers: {'Authorization': 'Bearer ' + accessToken}
+        }).then(response => response.json())
+        .then(data => this.setState({
+            user: {
+                name: data.display_name
+            }
+        }))
+
+        fetch('https://api.spotify.com/v1/me/playlists', {
+            headers: {'Authorization': 'Bearer ' + accessToken}
+        }).then(response => response.json())
+        .then(data => this.setState({
+            playlists: data.items.map(item => {
+                console.log(data.items);
+                return {
+                    name: item.name,
+                    // imageUrl: item.images.find(image => image.width == 640).url,
+                    imageUrl: item.images[0].url,
+                    songs: []
+                };
+            })
+        }))
     }
-  render() {
-    let playlistToRender = this.state.serverData.user ? this.state.serverData.user.playlists
-        .filter(playlist =>
-            playlist.name.toLowerCase().includes(
-                this.state.filterString.toLowerCase()
-            )
-        ) : [];
-    return (
-      <div className="App">
-          {this.state.serverData.user ?
-          <div>
-              <h1 style={{...defaultStyle, fontSize: '54px'}}>
-                  {this.state.serverData.user.name}'s Playlists
-              </h1>
-              <PlaylistCounter playlists={playlistToRender}/>
-              <HoursCounter playlists={playlistToRender}/>
-              <Filter onTextChange={text =>
-                  this.setState({filterString: text})
-              }/>
-              {playlistToRender.map(playlist =>
-                  <Playlist playlist={playlist}/>
-              )}
-          </div> : <h1 style={defaultStyle}>Loading...</h1>
-          }
-      </div>
-    );
-  }
+
+    render() {
+        let playlistToRender =
+            this.state.user &&
+            this.state.playlists
+                ? this.state.playlists.filter(playlist =>
+                  playlist.name.toLowerCase().includes(
+                      this.state.filterString.toLowerCase()))
+                : [];
+        return (
+          <div className="App">
+              {this.state.user ?
+              <div>
+                  <h1 style={{...defaultStyle, fontSize: '54px'}}>
+                      {this.state.user.name}'s Playlists
+                  </h1>
+                  <PlaylistCounter playlists={playlistToRender}/>
+                  <HoursCounter playlists={playlistToRender}/>
+                  <Filter onTextChange={text => {
+                      this.setState({filterString: text})
+                  }}/>
+                  {playlistToRender.map(playlist =>
+                      <Playlist playlist={playlist} />
+                  )}
+              </div> : <button onClick={() => window.location = 'http://localhost:8888/login'}
+                   style={{padding: '20px', fontSize: '50px', marginTop: '20px'}}>Sign in with Spotify</button>
+              }
+          </div>
+        );
+    }
 }
 
 export default App;
