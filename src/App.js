a795b15882dce886d147bb5d0a31c130ef3321f3
Wei Ern <ng.wei.ern@gmail.com>
2018-06-16 23:02:22 +0800

    changed to window.location.href.includes

--- src/App.js
+++ src/App.js
@@ -166,7 +166,7 @@ class App extends Component {
                       <Playlist playlist={playlist} />
                   )}
               </div> : <button onClick={() => {
-                  window.location = window.location.includes('localhost')
+                  window.location = window.location.href.includes('localhost')
                       ? 'http://localhost:8888/login'
                       : 'https://playlists-react.herokuapp.com/login' }
                 }
