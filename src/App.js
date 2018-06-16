4aec067e645aff29b40ff613325098f850e6b0e0
Wei Ern <ng.wei.ern@gmail.com>
2018-06-16 22:55:07 +0800

    Fixed button to also work in production

--- src/App.js
+++ src/App.js
@@ -165,8 +165,12 @@ class App extends Component {
                   {playlistToRender.map(playlist =>
                       <Playlist playlist={playlist} />
                   )}
-              </div> : <button onClick={() => window.location = 'http://localhost:8888/login'}
-                   style={{padding: '20px', fontSize: '50px', marginTop: '20px'}}>Sign in with Spotify</button>
+              </div> : <button onClick={() => {
+                  window.location = window.location.includes('localhost')
+                      ? 'http://localhost:8888/login'
+                      : 'https://playlists-react.herokuapp.com/login' }
+                }
+                style={{padding: '20px', fontSize: '50px', marginTop: '20px'}}>Sign in with Spotify</button>
               }
           </div>
         );
